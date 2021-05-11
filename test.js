30640cbe0971bdf54057b7ab4b837a9464b7c61c
eyal <azran4u@gmail.com>
2021-05-11 11:42:10 +0300

    first commit

--- test.js
+++ test.js
@@ -3,12 +3,24 @@ import http from "k6/http";
 export let options = {
   discardResponseBodies: true,
   scenarios: {
-    contacts: {
+    // my_awesome_api_test: {
+    //   executor: "constant-vus",
+    //   vus: 10,
+    //   duration: "1m",
+    // },
+    fixed_number_of_users: {
       executor: "per-vu-iterations",
-      vus: 10,
-      iterations: 2000,
-      maxDuration: "1m",
+      vus: 1000,
+      iterations: 100,
+      maxDuration: "5m",
     },
+    // contacts: {
+    //   executor: "constant-arrival-rate",
+    //   rate: 200, // 200 RPS, since timeUnit is the default 1s
+    //   duration: "1m",
+    //   preAllocatedVUs: 50,
+    //   maxVUs: 100,
+    // },
   },
   thresholds: {
     // We want the 95th percentile of all HTTP request durations to be less than 500ms
